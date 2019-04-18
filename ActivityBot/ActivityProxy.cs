3fdcdfbf722d2222e8933921dffd339189eb9c5d
Red Folder <markbryantaylor@yahoo.co.uk>
2019-04-18 17:02:32 +0100

    Testing MSI integration

--- ActivityBot/ActivityProxy.cs
+++ ActivityBot/ActivityProxy.cs
@@ -11,22 +11,27 @@ namespace ActivityBot
         public ActivityProxy(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            _httpClient.DefaultRequestHeaders.Add("Secret", Environment.GetEnvironmentVariable("MSI_SECRET"));
         }
 
         public async Task<string> GetAwaiting()
         {
             try
             {
-                var response = await _httpClient.GetAsync("https://rfc-activity.azurewebsites.net/api/GetPendingApprovals");
+                string resource = "https://rfc-activity.azurewebsites.net/api/GetPendingApprovals";
+                string apiversion = "2017-09-01";
 
-                if (response.IsSuccessStatusCode)
-                {
-                    var json = await response.Content.ReadAsStringAsync();
+                var response = await _httpClient.GetAsync(String.Format("{0}/?resource={1}&api-version={2}", Environment.GetEnvironmentVariable("MSI_ENDPOINT"), resource, apiversion));
+                //var response = await _httpClient.GetAsync("https://rfc-activity.azurewebsites.net/api/GetPendingApprovals");
 
-                    return json;
-                }
+                //if (response.IsSuccessStatusCode)
+                //{
+                    var content = await response.Content.ReadAsStringAsync();
+                //
+                //    return json;
+                //}
 
-                return $"Received status code: {response.StatusCode}";
+                return $"Received status code: {response.StatusCode} - {content}";
             }
             catch (Exception ex)
             {
