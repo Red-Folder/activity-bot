70042db297691b3852e7844a301b03bd426b2a2c
Mark Taylor <mark.taylor@Betfred.com>
2019-04-29 11:11:57 +0100

    Moved the bot configuration from .bot file to settings (as per Microsoft .bot deprecation advice)

--- ActivityBot/Startup.cs
+++ ActivityBot/Startup.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 using ActivityBot.Activity.Proxy;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Configuration;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ActivityBot
 {
@@ -30,7 +22,6 @@ namespace ActivityBot
             _isProduction = env.IsProduction();
             Configuration = configuration;
             _httpClient = new HttpClient();
-
         }
 
         public IConfiguration Configuration { get; }
@@ -41,45 +32,6 @@ namespace ActivityBot
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddTransient(x => new ActivityProxy(_httpClient, Configuration.GetSection("GetPendingApprovalsUrl")?.Value));
-
-            var secretKey = Configuration.GetSection("botFileSecret")?.Value;
-            var botFilePath = Configuration.GetSection("botFilePath")?.Value;
-            if (!File.Exists(botFilePath))
-            {
-                throw new FileNotFoundException($"The .bot configuration file was not found. botFilePath: {botFilePath}");
-            }
-
-            // Loads .bot configuration file and adds a singleton that your Bot can access through dependency injection.
-            BotConfiguration botConfig = null;
-            try
-            {
-                botConfig = BotConfiguration.Load(botFilePath, secretKey);
-            }
-            catch
-            {
-                var msg = @"Error reading bot file. Please ensure you have valid botFilePath and botFileSecret set for your environment.
-        - You can find the botFilePath and botFileSecret in the Azure App Service application settings.
-        - If you are running this bot locally, consider adding a appsettings.json file with botFilePath and botFileSecret.
-        - See https://aka.ms/about-bot-file to learn more about .bot file its use and bot configuration.
-        ";
-                throw new InvalidOperationException(msg);
-            }
-
-            services.AddSingleton(sp => botConfig ?? throw new InvalidOperationException($"The .bot configuration file could not be loaded. botFilePath: {botFilePath}"));
-
-            // Retrieve current endpoint.
-            var environment = _isProduction ? "production" : "development";
-            var service = botConfig.Services.FirstOrDefault(s => s.Type == "endpoint" && s.Name == environment);
-            if (service == null && _isProduction)
-            {
-                // Attempt to load development environment
-                service = botConfig.Services.Where(s => s.Type == "endpoint" && s.Name == "development").FirstOrDefault();
-            }
-
-            if (!(service is EndpointService endpointService))
-            {
-                throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
-            }
 
             // Memory Storage is for local bot debugging only. When the bot
             // is restarted, everything stored in memory will be gone.
@@ -107,9 +59,11 @@ namespace ActivityBot
             var conversationState = new ConversationState(dataStore);
             services.AddSingleton(conversationState);
 
+            var microsoftBotApplicationId = Configuration.GetSection("MicrosoftBotApplicationId")?.Value;
+            var microsoftBotApplicationPassword = Configuration.GetSection("MicrosoftBotApplicationPassword")?.Value;
             services.AddBot<Activity.ActivityBot>(options =>
             {
-                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+                options.CredentialProvider = new SimpleCredentialProvider(microsoftBotApplicationId, microsoftBotApplicationPassword);
 
                 // Catches any errors that occur during a conversation turn and logs them to currently
                 // configured ILogger.
